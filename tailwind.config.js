fb65cd6a6e4bae2ece93807da8c8542ab8094b25
Jahir514 <jahirhasan.amana@gmail.com>
2023-09-11 17:24:36 +0600

    featrue section done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./src/*.{html,js}", "./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['josefin Sans', 'sans-serif'],
+        alata: ['Alata']
+      }
+    },
+  },
+  plugins: [],
+}
+
